f1ddbb647ad7c5f2a4bff3526f21bce26e302816
Ezra Paz <jrpaz@bayad.com>
2022-05-29 23:37:13 +0800

    chore: remove unused configs

--- src/config/env.config.js
+++ src/config/env.config.js
@@ -4,8 +4,6 @@ dotenv.config()
 exports.config = {
     port: process.env.PORT,
     env: process.env.ENV,
-    secret: process.env.APP_SECRET,
-    hostname: process.env.HOSTNAME,
     mongo: {
         uri: process.env.MONGOURI,
         testURI: process.env.MONGOTESTURI
