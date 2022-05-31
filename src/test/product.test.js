aa440270948968b55a52e287ec3c8865d06f9c62
Ezra Paz <jrpaz@bayad.com>
2022-05-31 12:03:15 +0800

    feat: add tests to products test suite

--- src/test/product.test.js
+++ src/test/product.test.js
@@ -16,7 +16,7 @@ describe("GET /products", () => {
                 .send()
             console.log(response.statusCode)
             await expect(response.statusCode).toBe(200)
-        })
+        }, 30000)
 
         test("should return 401 when authorization header is not provided", async () => {
             const response = await request(app)
@@ -24,12 +24,12 @@ describe("GET /products", () => {
                 .send()
             console.log(response.statusCode)
             await expect(response.statusCode).toBe(401)
-        })
+        }, 10000)
     })
 })
 
 describe("POST /products", () => {
-    describe("Get all products", () => {
+    describe("Create new product", () => {
         test("should return with a 200 status code", async () => {
             const auth = await request(app).post("/auth/login").send({
                 "email": "merchant@gmail.com",
@@ -45,7 +45,7 @@ describe("POST /products", () => {
                 })
             console.log(response.statusCode)
             await expect(response.statusCode).toBe(200)
-        })
+        }, 10000)
 
         test("should return 401 when authorization header is not provided", async () => {
             const response = await request(app)
@@ -57,7 +57,7 @@ describe("POST /products", () => {
                 })
             console.log(response.statusCode)
             await expect(response.statusCode).toBe(401)
-        })
+        }, 10000)
 
         test("should block access when user role is not merchant", async () => {
             const auth = await request(app).post("/auth/login").send({
@@ -74,6 +74,101 @@ describe("POST /products", () => {
                 })
             console.log(response.statusCode)
             await expect(response.statusCode).toBe(403)
-        })
+        }, 10000)
+    })
+})
+
+describe("PUT /products/:id", () => {
+    describe("Update existing product", () => {
+        test("should return with a 200 status code", async () => {
+            const auth = await request(app).post("/auth/login").send({
+                "email": "merchant@gmail.com",
+                "password": "merchantpassword"
+            })
+            const response = await request(app)
+                .put("/products/6294ed5b7f175f988473518e")
+                .set("Authorization", `Bearer ${auth.body.token}`)
+                .send({
+                    "name": faker.commerce.productName(),
+                    "price": faker.datatype.float({ max: 100 })
+                })
+            console.log(response.statusCode)
+            await expect(response.statusCode).toBe(200)
+        }, 10000)
+
+        test("should return 401 when authorization header is not provided", async () => {
+            const response = await request(app)
+                .put("/products/6294ed5b7f175f988473518e")
+                .send({
+                    "name": faker.commerce.productName(),
+                    "price": faker.datatype.float({ max: 100 })
+                })
+            console.log(response.statusCode)
+            await expect(response.statusCode).toBe(401)
+        }, 10000)
+
+        test("should block access when user role is not merchant", async () => {
+            const auth = await request(app).post("/auth/login").send({
+                "email": "customer@gmail.com",
+                "password": "customerpassword"
+            })
+            const response = await request(app)
+                .put("/products/6294ed5b7f175f988473518e")
+                .set("Authorization", `Bearer ${auth.body.token}`)
+                .send({
+                    "name": faker.commerce.productName(),
+                    "price": faker.datatype.float({ max: 100 })
+                })
+            console.log(response.statusCode)
+            await expect(response.statusCode).toBe(403)
+        }, 10000)
+    })
+})
+
+describe("DELETE /products/:id", () => {
+    describe("Delete existing product", () => {
+        test("should return with a 200 status code", async () => {
+            const auth = await request(app).post("/auth/login").send({
+                "email": "merchant@gmail.com",
+                "password": "merchantpassword"
+            })
+            const products = await request(app)
+                .get("/products")
+                .set("Authorization", `Bearer ${auth.body.token}`)
+                .send()
+            let lastProduct = products.body.data.pop()
+            const response = await request(app)
+                .delete(`/products/${lastProduct._id}`)
+                .set("Authorization", `Bearer ${auth.body.token}`)
+                .send()
+            console.log(response.statusCode)
+            await expect(response.statusCode).toBe(200)
+        }, 10000)
+
+        test("should return 401 when authorization header is not provided", async () => {
+            const response = await request(app)
+                .delete(`/products/test`)
+                .send()
+            console.log(response.statusCode)
+            await expect(response.statusCode).toBe(401)
+        }, 10000)
+
+        test("should block access when user role is not merchant", async () => {
+            const auth = await request(app).post("/auth/login").send({
+                "email": "customer@gmail.com",
+                "password": "customerpassword"
+            })
+            const products = await request(app)
+                .get("/products")
+                .set("Authorization", `Bearer ${auth.body.token}`)
+                .send()
+            let lastProduct = products.body.data.pop()
+            const response = await request(app)
+                .delete(`/products/${lastProduct._id}`)
+                .set("Authorization", `Bearer ${auth.body.token}`)
+                .send()
+            console.log(response.statusCode)
+            await expect(response.statusCode).toBe(403)
+        }, 10000)
     })
 })
